6c505e43654c5e70c9e0dfb04c70cd4c6f0ca015
Cameron Dilger <cloud.cameronxdx@gmail.com>
2024-09-08 13:10:19 -0400

    lambda

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,7 +59,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch(
-        "https://cameron-cloud-resume-challenge.s3.amazonaws.com/index.html"
+        "https://gncjh6hb7rfber4acvy2l5o2bm0juigo.lambda-url.us-east-1.on.aws/"
     );
     let data = await response.json();
     counter.innerHTML = `👀 Views: ${data}`;
