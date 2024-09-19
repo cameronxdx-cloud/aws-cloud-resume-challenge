62ac82aa8ce29e63914967fd0a0ba9333b3ad7ec
Cameron Dilger <cloud.cameronxdx@gmail.com>
2024-09-19 10:33:55 -0400

    Logo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,3 @@
-// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-//     anchor.addEventListener('click', function (e) {
-//         e.preventDefault();
-
-//         document.querySelector(this.getAttribute('href')).scrollIntoView({
-//             behavior: 'smooth'
-//         });
-//     });
-// });
-
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://gncjh6hb7rfber4acvy2l5o2bm0juigo.lambda-url.us-east-1.on.aws/");
